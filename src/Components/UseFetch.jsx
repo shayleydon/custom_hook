a2af3314fe24babbeda4ee147f83dedf82a342e9
shayleydon <shayleydon>
2025-03-25 22:49:57 +0000

    create fetch data hook, useFetch

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,12 @@
-const UseFetch = (url) => {
-    
+import {useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const[data,setData]=useState();
+    useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+return [data]
 }
 
-export default UseFetch
+export default useFetch
